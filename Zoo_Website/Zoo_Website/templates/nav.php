30ddd0b2a6f4222b56517fd9fdb3023038634f29
Thom Rawden <24005959@student.stamford.ac.uk>
2025-11-28 12:13:27 +0000

    Information Template

--- Zoo_Website/Zoo_Website/templates/nav.php
+++ Zoo_Website/Zoo_Website/templates/nav.php
@@ -1,7 +1,7 @@
 <nav class="nav">
     <a href="#">Educational Visits</a>
     <a href="#">Booking</a>
-    <a href="#">Info</a>
+    <a href="../public/information.php">Info</a>
     <a href="#">Reviews</a>
     <a href="#">Login / Sign up</a>
 </nav>
